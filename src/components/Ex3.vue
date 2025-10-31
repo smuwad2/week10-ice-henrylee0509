d4d627bd831b9cda8a073186b86f0c192b4e8fe1
henrylee0509 <henry.lee.2024@computing.smu.edu.sg>
2025-10-31 15:53:24 +0000

    ex3

--- src/components/Ex3.vue
+++ src/components/Ex3.vue
@@ -1,18 +1,20 @@
 <script setup>
-    // Import BlogPost component
+    // Import BlogPost component 
+    //
     import blogPost from './subcomponents/BlogPost2.vue'
-	import axios from 'axios'
+    import axios from 'axios'
 </script>
+
 
 <script>
     export default {
+        components: {
+            blogPost
+        },
         data() {
             return {
                 posts: [] // array of post objects
             }  
-        },
-        components:{
-            blogPost
         },
         computed: {
             baseUrl() {
@@ -20,11 +22,11 @@
                     return 'http://localhost:3000'
                 else {
                     const codespace_host = window.location.hostname.replace('5173', '3000')
-                    return `https://${codespace_host}`;
+                    return https://${codespace_host};
                 }
             }
         },
-        created() { // created is a hook that executes as soon as Vue instance is created
+        created() { // [translate:created is a hook that executes as soon as Vue instance is created]
             axios.get(`${this.baseUrl}/posts`)
             .then(response => {
                 // this gets the data, which is an array
@@ -38,31 +40,27 @@
         methods: {
             deletePost(id) {
                 // TODO: Complete the delete method
-                axios.get(`${this.baseUrl}/deletePost`,{
-                    params: {
-                        id: id
-                    }
-                }).then(response=>{
-                    console.log(response.data.message)
-                    this.posts = this.posts.filter(post => post.id != id)
-                    // for (let i = 0; i < this.posts.length; i++) {
-                    //     if (this.posts[i].id === id) {
-                    //         this.posts.splice(i, 1);
-                    //         break;
-                    //     }
-                    // }
-                }).catch(error =>{
-                    console.log(error)
+                axios.get(`${this.baseUrl}/deletePost`, {params: {
+                    id: id
+                }})
+                .then(response => {
+                    // this gets the data, which is an array
+                    console.log(id)
+                    this.posts = this.posts.filter(post=>post.id!=id)
+                    console.log(response.data)
+                })
+                .catch(error => {
+                    this.posts = [{ entry: 'There was an error: ' + error.message }]
                 })
             }
         }
     }
 </script>
 
+
 <template>
    <!-- TODO: make use of the 'blog-post' component to display the blog posts -->
-    <blogPost v-for="post in posts" :subject="post.subject" :entry="post.entry" :mood="post.mood">
-        <button class="btn btn-primary" @click="deletePost(post.id)">Delete</button>
-    </blogPost>
+<blogPost v-for="v in this.posts" :key="v.id" :id="v.id" :mood="v.mood" :subject="v.subject" :entry="v.entry">
+    <button class="btn btn-primary" @click="deletePost(v.id)">Delete</button>
+</blogPost>
 </template>
-
